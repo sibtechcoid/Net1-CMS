33f44996cfb38472c56d83f08d39b7b8197e0f5f
Sarif Sidik <sidik.sarif@yahoo.com>
2020-01-28 23:41:43 +0700

    Create BannersAPI CRUD

---
+++ app/Http/Controllers/Api/BannersNetOneApiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\ApiModels\BannersNetOneApi;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BannersNetOneApiController extends Controller
+{
+    //send data from database to mobile
+    public function Banners(){
+        $banners= BannersNetOneApi::all();
+        $send = json_decode($banners,true);
+        return $send;
+
+    }
+}
