8171f59054920fc5d15c1750cefe9edc45881450
RoyDesktop <roy.vitoraja@gmail.com>
2020-02-03 15:09:37 +0700

    added products download as excel

--- app/Models/Admin/Product.php
+++ app/Models/Admin/Product.php
@@ -15,6 +15,8 @@ class Product extends Model
    
     protected $fillable = ['offer_id','offer_name','display_name','description','charging_type','offer_type','service_zone','total_price','validity_date',];
 
+    protected $hidden = ['created_at', 'updated_at'];
+
     public function getTableColumns() {
         return $this->getConnection()->getSchemaBuilder()->getColumnListing($this->getTable());
     }
